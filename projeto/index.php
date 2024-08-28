376604bfbe5f82f572af591c8c8ac4a018f9ca42
fmacedosantos <felipemacedo986@gmail.com>
2024-08-28 11:19:10 -0300

    Começando o projeto

---
+++ projeto/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+</head>
+<?php
+    $plataforma = "DIO";
+?>
+<body>
+    <h1>Curso sobre tipos em PHP</h1>
+    <h4>O curso foi feito pela 
+        <?php
+            echo $plataforma;
+        ?>
+    </h4>
+    
+</body>
+</html>
